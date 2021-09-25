96b84e6634a924e91f51a8cfcdcfaf97f9aa26c0
nblenke <nathan@blenke.com>
2021-09-25 12:46:22 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,70 +1,27 @@
-# Getting Started with Create React App
+# WebXR Immersive AR
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Based on [Create an immersive AR session using WebXR](https://developers.google.com/ar/develop/webxr/hello-webxr)
 
-## Available Scripts
 
-In the project directory, you can run:
+## Related links
+- [WebXR W3C Spec](https://www.w3.org/TR/webxr/)
+- [MDN WebXR_Device_API](https://developer.mozilla.org/en-US/docs/Web/API/WebXR_Device_API)
+- [The Immersive Web Working Group/Community Group](https://immersive-web.github.io/)
+- [three.js](https://threejs.org/docs/index.html)
+- [WebXR Polyfill](https://github.com/immersive-web/webxr-polyfill)
+- [WebXR Emulator](https://blog.mozvr.com/webxr-emulator-extension-ar-support/)
+- [Remote debug Android devices](https://developer.chrome.com/docs/devtools/remote-debugging/)
+- [Create React App](https://github.com/facebook/create-react-app)
 
-### `yarn start`
+## Setup
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+1. Install deps and run secure local server
+```
+yarn install
+```
+```
+HTTPS=true yarn start
+```
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+2. Navigate to `https://[your ip]:3000` on your phone
+3. Click start, place reticle and tap screen to place node
--- src/App.js
+++ src/App.js
@@ -1,25 +1,187 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import * as THREE from 'three'
+// import WebXRPolyfill from 'webxr-polyfill'
+
+import styled from 'styled-components'
+
+const Foot = styled.div`
+    position fixed;
+    height: 50px;
+    width: 100vw;
+    padding: 10px;
+    bottom: 0;
+`
+
+const {xr} = navigator
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // TODO: Geolocation
+    // const [coords, setCoords] = React.useState({})
+    // const init = async () => {
+    //     navigator.geolocation.getCurrentPosition(
+    //         (pos) => {
+    //             const coords = pos.coords
+    //
+    //             setCoords({
+    //                 lat: coords.latitude,
+    //                 lon: coords.longitude,
+    //             })
+    //
+    //             console.log(`==coords ${coords.latitude} ${coords.longitude}`)
+    //         },
+    //         (err) => {
+    //             console.warn(`ERROR(${err.code}): ${err.message}`)
+    //         }
+    //     )
+    // }
+    // React.useEffect(() => {
+    //     init()
+    // }, [])
+
+    const activateXR = async () => {
+        const immersiveOK = await xr.isSessionSupported('immersive-ar')
+
+        if (!immersiveOK) {
+            return
+        }
+        // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
+        const canvas = document.getElementById('canvas')
+        const gl = canvas.getContext('webgl', {xrCompatible: true})
+        const scene = new THREE.Scene()
+
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3)
+        directionalLight.position.set(10, 15, 10)
+        scene.add(directionalLight)
+
+        const particleLight = new THREE.Mesh(
+            new THREE.SphereGeometry(4, 8, 8),
+            new THREE.MeshBasicMaterial({color: 0xffffff})
+        )
+        scene.add(particleLight)
+
+        // Set up the WebGLRenderer, which handles rendering to the session's base layer.
+        const renderer = new THREE.WebGLRenderer({
+            alpha: true,
+            preserveDrawingBuffer: true,
+            canvas: canvas,
+            context: gl,
+        })
+        renderer.autoClear = false
+
+        // The API directly updates the camera matrices.
+        // Disable matrix auto updates so three.js doesn't attempt
+        // to handle the matrices independently.
+        const camera = new THREE.PerspectiveCamera()
+        camera.matrixAutoUpdate = false
+
+        // Initialize a WebXR session using "immersive-ar".
+        // const session = await xr.requestSession('immersive-ar')
+        const session = await xr.requestSession('immersive-ar', {
+            requiredFeatures: ['hit-test'],
+            optionalFeatures: ['dom-overlay'],
+            domOverlay: {
+                root: document.getElementById('xr-overlay'),
+            },
+        })
+        session.updateRenderState({
+            baseLayer: new window.XRWebGLLayer(session, gl),
+        })
+
+        // A 'local' reference space has a native origin that is located
+        // near the viewer's position at the time the session was created.
+        const referenceSpace = await session.requestReferenceSpace('local') // local
+
+        // Create another XRReferenceSpace that has the viewer as the origin.
+        const viewerSpace = await session.requestReferenceSpace('viewer')
+        // Perform hit testing using the viewer as origin.
+        const hitTestSource = await session.requestHitTestSource({
+            space: viewerSpace,
+        })
+
+        const cubeGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
+        const cubeMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00})
+        const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+
+        const reticleGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
+        const reticleMaterial = new THREE.MeshBasicMaterial({
+            color: 0xff0000,
+        })
+        const reticle = new THREE.Mesh(reticleGeometry, reticleMaterial)
+        reticle.visible = true
+        scene.add(reticle)
+
+        session.addEventListener('select', (event) => {
+            if (cube) {
+                const clone = cube.clone()
+                clone.position.copy(reticle.position)
+                scene.add(clone)
+            }
+        })
+
+        // Create a render loop that allows us to draw on the AR view.
+        const onXRFrame = (time, frame) => {
+            // Queue up the next draw request.
+            session.requestAnimationFrame(onXRFrame)
+
+            // Bind the graphics framebuffer to the baseLayer's framebuffer
+            gl.bindFramebuffer(
+                gl.FRAMEBUFFER,
+                session.renderState.baseLayer.framebuffer
+            )
+
+            // Retrieve the pose of the device.
+            // XRFrame.getViewerPose can return null while the session attempts to establish tracking.
+            const pose = frame.getViewerPose(referenceSpace)
+            if (pose) {
+                // In mobile AR, we only have one view.
+                const view = pose.views[0]
+
+                const viewport = session.renderState.baseLayer.getViewport(view)
+                renderer.setSize(viewport.width, viewport.height)
+
+                // Use the view's transform matrix and projection matrix to configure the THREE.camera.
+                camera.matrix.fromArray(view.transform.matrix)
+                camera.projectionMatrix.fromArray(view.projectionMatrix)
+                camera.updateMatrixWorld(true)
+
+                const hitTestResults = frame.getHitTestResults(hitTestSource)
+                if (hitTestResults.length > 0 && reticle) {
+                    const hitPose = hitTestResults[0].getPose(referenceSpace)
+                    reticle.visible = true
+                    reticle.position.set(
+                        hitPose.transform.position.x,
+                        hitPose.transform.position.y,
+                        hitPose.transform.position.z
+                    )
+                    reticle.updateMatrixWorld(true)
+                }
+
+                // Render the scene with THREE.WebGLRenderer.
+                renderer.render(scene, camera)
+            }
+        }
+        session.requestAnimationFrame(onXRFrame)
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <canvas id="canvas" />
+            </div>
+            <Foot id="xr-overlay">
+                <button
+                    onClick={() => {
+                        window.location.reload()
+                    }}
+                >
+                    RELOAD
+                </button>
+            </Foot>
+
+            <button onClick={() => activateXR()}>START</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
